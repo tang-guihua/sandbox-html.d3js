6add07da4b7e186351088f284c5dc960ac6c9172
tang-guihua <tl301@126.com>
2022-06-05 17:40:22 +0800

    animation backup

--- js/draw-scatter.js
+++ js/draw-scatter.js
@@ -1,6 +1,6 @@
 async function x() {
     const pathToJSON = await d3.json("../data/seattle_wa_weather_data.json")
-    // console.log(pathToJSON[1])
+    // console.log(pathToJSON[20])
 
     const xAccessor = d => d.dewPoint
     const yAccessor = d => d.humidity
@@ -44,44 +44,53 @@ async function x() {
         .domain(d3.extent(pathToJSON, yAccessor))
         .range([dimensions.boundedHeight, 0])
         .nice()
-
-    // bounds.append('circle')
-    //     .attr('cx', dimensions.boundedWidth / 2)
-    //     .attr('cy', dimensions.boundedHeight / 2)
-    //     .attr('r', 5)
-    
-    // pathToJSON.forEach(d => {
-    //   bounds
-    //     .append('circle')
-        // .attr('cx', xScale(xAccessor(d)))
-        // .attr('cy', yScale(yAccessor(d)))
-        // .attr('r', 5)
-    // })
-
-    // const dots = bounds.selectAll('circle')
-    //     .data(pathToJSON)
-    //     .enter()
-    //     .append('circle')
-    //     .attr('cx', d => xScale(xAccessor(d)))
-    //     .attr('cy', d => yScale(yAccessor(d)))
-    //     .attr('r', 3)
-    //     .attr("fill", "cornflowerblue")
     
     function drawDots(dataSet, color) {
       const dots = bounds.selectAll('circle')
         .data(dataSet)
-        .enter()
-        .append('circle')
+        dots
+        .join('circle')
         .attr('cx', d => xScale(xAccessor(d)))
         .attr('cy', d => yScale(yAccessor(d)))
         .attr('r', 3)
-        .attr('fill', color)
+        .attr('fill', d => colorRange(colorAccess(d)))
     }
-    drawDots(pathToJSON.slice(0, 200), 'darkgrey')
+    
 
-    setTimeout(() => {
-      drawDots(pathToJSON, "cornflowerblue")
-    }, 1000);
+    const xAxisGenerator = d3.axisBottom()
+        .scale(xScale)
+    const xAxis = bounds.append('g')
+        .call(xAxisGenerator)
+        .style('transform', `translateY(${dimensions.boundedHeight}px)`)
+
+    const yAxisGenerator = d3.axisLeft()
+        .scale(yScale)
+    const yAxis = bounds.append('g')
+        .call(yAxisGenerator)
+    
+    const xText = xAxis.append('text')
+        .attr('x', dimensions.boundedWidth / 2)
+        .attr('y', dimensions.margin.bottom - 15)
+        .attr('fill', 'black')
+        .style('font-size', '1.2em')
+        .html('Dew point (def)')
+    
+    const yText = yAxis.append('text')
+        .attr('y', -dimensions.margin.left + 10)
+        .attr('x', -dimensions.boundedHeight / 2)
+        .attr('fill', 'black')
+        .attr('font-size', '1.2em')
+        .html('Relative humidity')
+        .style('transform', 'rotate(-90deg)')
+        .style('text-anchor', 'middle')
+
+    const colorAccess = d => d.cloudCover
+
+    const colorRange = d3.scaleLinear()
+        .domain(d3.extent(pathToJSON, colorAccess))
+        .range(["skyblue", "darkslategrey"])
+
+    drawDots(pathToJSON)
 }
 
 x()
